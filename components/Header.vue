d5be0bb631acdd9a02061db852489c8f29a4d634
AzPogoDev <henri@git.com>
2021-05-10 15:10:13 +0200

    Commit

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header container">
+      <div class="header__logo">
+          <nuxt-link to="/">
+              <img :src="'/static/img/templogo.png'">
+              </nuxt-link>
+      </div>
+      <div class="header__nav"></div>
+      <div class="header__buttons">
+
+      </div>
+
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
